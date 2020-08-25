22b26a0615f80c41e6e7d44b99a189530269a955
datejer <ejer.priv@wp.pl>
2020-08-25 12:09:31 +0200

    Add help command (alias of p5 with no cmd), add p5 -v or -version that checks for updates and automatic update checking on new project

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const chalk = require("chalk");
 const inquirer = require("inquirer");
 const git = require("nodegit");
 const fs = require("fs");
+const fetch = require("node-fetch");
+
+const package = require("./package.json");
 
 const bootstrap = (name) => {
     console.log(chalk.green.bold("+ ") + chalk.white(`Creating new p5.js project in ${chalk.white.bold(name)}...`));
@@ -14,11 +17,23 @@ const bootstrap = (name) => {
             console.log(chalk.green.bold(`\nSuccessfully created an empty p5.js sketch inside of the ${chalk.white.bold(name)} directory!`));
             console.log(chalk.white.bold(`Get started by going into the repository ${chalk.gray(`(cd ${name})`)} and writing your sketch.`));
             console.log(chalk.white.bold(`To run the sketch we recommend using the ${chalk.green.bold("serve")} package ${chalk.gray(`(npm i -g serve && serve)`)}\n`));
+            checkVersion();
         });
     }).catch(e => console.log(e));
 }
 
-if (process.argv.length <= 2) {
+const checkVersion = () => {
+    fetch('https://registry.npmjs.org/p5js-cli')
+        .then(res => res.json())
+        .then(json => {
+            if (parseInt(package.version.replace(/\./gi, "")) < parseInt(json["dist-tags"].latest.replace(/\./gi, ""))) {
+                console.log(chalk.red.bold("\n! ") + chalk.white("Update available: ") + chalk.green.bold(json["dist-tags"].latest));
+                console.log(chalk.red.bold("! ") + chalk.white("Run ") + chalk.cyan("npm install -g p5js-cli") + chalk.white(" to update\n"));
+            }
+        });
+}
+
+if (process.argv.length <= 2 || process.argv[2] === "help") {
     console.log(chalk.white.bold(`\nWelcome to the unofficial p5.js command line tool!`));
     console.log(chalk.white("With it you can easily bootstrap new p5.js sketches using just your command line.\n"));
     console.log(chalk.white("You can get started by typing:\n    p5 new \<project name\>\n"));
@@ -51,5 +66,8 @@ if (process.argv.length <= 2) {
             projectName = args.join("-").toLowerCase();
             bootstrap(projectName);
         }
+    } else if (command === "-v" || command === "-version") {
+        console.log(`p5.js CLI Version: ${package.version}`);
+        checkVersion();
     }
 }
